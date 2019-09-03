97b0d36b02d967d2314d56df31ec97c5d9ac6e47
Ishwant Singh <ishwant8@gmail.com>
2019-09-04 03:28:37 +0530

    feat: timer added. update: margins and width tweaked

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // js
+
+//timer
+const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+
+let countDown = new Date("Oct 17, 2019 00:00:00").getTime(),
+  x = setInterval(function() {
+    let now = new Date().getTime(),
+      distance = countDown - now;
+
+    (document.getElementById("days").innerText = Math.floor(distance / day)),
+      (document.getElementById("hours").innerText = Math.floor(
+        (distance % day) / hour
+      )),
+      (document.getElementById("minutes").innerText = Math.floor(
+        (distance % hour) / minute
+      )),
+      (document.getElementById("seconds").innerText = Math.floor(
+        (distance % minute) / second
+      ));
+
+    //   when timer is reached
+    if (distance < 0) {
+      clearInterval(x);
+      ("BVEST IS HERE");
+    }
+  }, second);
+
+//TIMER END
