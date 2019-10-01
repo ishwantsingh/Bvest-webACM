9a8a02bf8824fcc9050ee5209e4ba6629eb4aed1
Ishwant Singh <ishwant8@gmail.com>
2019-10-01 10:06:41 +0530

    updated: tech from rar file.

--- bvest-techs/script.js
+++ bvest-techs/script.js
@@ -1,25 +1,23 @@
-
 // Sticky menu background
-window.addEventListener('scroll', function() {
+window.addEventListener("scroll", function() {
   if (window.scrollY > 150) {
-    document.querySelector('#navbar').style.opacity = 0.9;
+    document.querySelector("#navbar").style.opacity = 0.9;
   } else {
-    document.querySelector('#navbar').style.opacity = 1;
+    document.querySelector("#navbar").style.opacity = 1;
   }
 });
 
-
 // Smooth Scrolling
 
-$('.nav-link').on('click', function(event) {
-  if (this.hash !== '') {
+$(".nav-link").on("click", function(event) {
+  if (this.hash !== "") {
     event.preventDefault();
 
     const hash = this.hash;
 
-    $('html, body').animate(
+    $("html, body").animate(
       {
-        scrollTop: $(hash).offset().top 
+        scrollTop: $(hash).offset().top
       },
       800
     );
